4c728444b69f36d1c577a2ef8bde5f96f549ad1d
Zadid13 <77864700+Zadid13@users.noreply.github.com>
2022-08-09 17:16:17 +0600

    Add files via upload

---
+++ TimeConversionuri.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    cout<<n/3600<<":"<<n%3600/60<<":"<<n%3600%60<<endl;
+    return 0;
+
+
+
+}
+//https://www.beecrowd.com.br/judge/en/problems/view/1019
