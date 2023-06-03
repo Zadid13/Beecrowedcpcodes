ce5b98828f5d4a5b15b753a165dce8cdc99a9d76
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-06-03 16:50:12 +0600

    Create  MultiplicationTableuri.cpp

---
+++ MultiplicationTableuri.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int n;
+     cin>>n;
+    cout<<"1"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<1*n<<endl;
+    cout<<"2"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<2*n<<endl;
+    cout<<"3"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<3*n<<endl;
+    cout<<"4"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<4*n<<endl;
+    cout<<"5"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<5*n<<endl;
+    cout<<"6"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<6*n<<endl;
+    cout<<"7"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<7*n<<endl;
+    cout<<"8"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<8*n<<endl;
+    cout<<"9"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<9*n<<endl;
+    cout<<"10"<<" "<<"x"<<" "<<n<<" "<<"="<<" "<<10*n<<endl;
+   
+    
+    return 0;
+}
